e6c614447636afd5e5e4e0d51aa85720af46f786
mkhammash <muhannad.khammash@hotmail.com>
2021-10-25 12:13:23 +0400

    Update scanImage.sh

--- files/scanImage.sh
+++ files/scanImage.sh
@@ -3,7 +3,7 @@
 curl -k -u $TL_USER:$TL_PASS --details --output ./twistcli https://$TL_CONSOLE/api/v1/util/twistcli
 chmod a+x ./twistcli
  
-docker pull pasqu4le/evilpetclinic:latest     
+docker pull mkhammash/evilpetclinic:latest     
 #curl -k -u $TL_USER:$TL_PASS --output ./twistcli https://$TL_CONSOLE/api/v1/util/twistcli
 ./twistcli images scan --u $TL_USER --p $TL_PASS --address https://$TL_CONSOLE --details pasqu4le/evilpetclinic -o output.json
 result=$(curl -k -u $TL_USER:$TL_PASS -H 'Content-Type: application/json' "https://$TL_CONSOLE/api/v1/scans?type=ciImage&collections=evilpetclinic&reverse=true&sort=time&limit=1"|jq '.[0].vulnFailureSummary'|grep "Scan failed")
